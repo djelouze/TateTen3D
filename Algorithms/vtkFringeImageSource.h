ffe5ecd78dfbb29eef47f7cadb13e73e32606744
jeromevelut <jerome.velut@gmail.com>
2010-02-04 21:55:55 +0100

    First commit

---
+++ Algorithms/vtkFringeImageSource.h
@@ -0,0 +1,11 @@
+//! \class vtkFringeImageSource
+//! \brief Create an image of fringe
+//!
+//!
+
+#include <vtkImageAlgorithm.h>
+
+class VTK_EXPORT vtkFringeImageSource : public vtkImageAlgorithm
+{
+
+}
